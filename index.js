7dd0133c7ffb8d5b7ca727b5f0fe598667746d0a
Joshua <joshuawilliams7800@gmail.com>
2022-10-15 19:24:08 -0500

    added operators, operator events, and number events

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const bgBtn = document.querySelector(".bg-display");
-const mainBtn = document.querySelector(".main-display");
+// QuerySelectors for Buttons
+const bgDisplay = document.querySelector(".bg-display");
+const mainDisplay = document.querySelector(".main-display");
 const clearBtn = document.querySelector(".clear");
 const ceBtn = document.querySelector(".ce");
 const equalsBtn = document.querySelector("equals");
@@ -18,8 +19,56 @@ const multiplyBtn = document.querySelector(".multiply");
 const divideBtn = document.querySelector(".divide");
 const subtractBtn = document.querySelector(".subtract");
 const addBtn = document.querySelector(".add");
+const numBtnArr = [
+  oneBtn,
+  twoBtn,
+  threeBtn,
+  fourBtn,
+  fiveBtn,
+  sixBtn,
+  sevenBtn,
+  eightBtn,
+  nineBtn,
+  zeroBtn,
+];
+const opBtnArr = [addBtn, subtractBtn, divideBtn, multiplyBtn];
 
+//Operators
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const mutliply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const operate = (a, operator, b) => {
+  switch (operator) {
+    case "+":
+      console.log(add(a, b));
+      break;
+    case "-":
+      console.log(subtract(a, b));
+      break;
+    case "*":
+      console.log(multiply(a, b));
+      break;
+    case "/":
+      console.log(divide(a, b));
+      break;
+  }
+};
+
+//Operator Events
+const numEventCreator = (btn) => {
+  btn.addEventListener("click", (e) => {
+    mainDisplay.textContent = `${mainDisplay.textContent}${e.target.textContent}`;
+  });
+};
+
+const opEventCreator = (btn) => {
+  btn.addEventListener("click", (e) => {
+    mainDisplay.textContent = `${mainDisplay.textContent} ${e.target.textContent} `;
+  });
+};
+
+numBtnArr.forEach((btn) => numEventCreator(btn));
+opBtnArr.forEach((btn) => opEventCreator(btn));
+
+//Clear, CE
