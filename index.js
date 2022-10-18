2a322dbde8f2275be28ffaafe26513e329ce4d17
Joshua <joshuawilliams7800@gmail.com>
2022-10-17 20:07:00 -0500

    added equals event, operation/calculation logic, and keyboard logic--

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // QuerySelectors for Buttons
+const whole = document.querySelector(".display");
 const bgDisplay = document.querySelector(".bg-display");
 const mainDisplay = document.querySelector(".main-display");
 const clearBtn = document.querySelector(".clear");
@@ -30,30 +31,91 @@ const numBtnArr = [
   eightBtn,
   nineBtn,
   zeroBtn,
+  decimalBtn,
 ];
 const opBtnArr = [addBtn, subtractBtn, divideBtn, multiplyBtn];
 
-//Operators
+//Operators and operation logic
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
-const mutliply = (a, b) => a * b;
+const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
 const operate = (a, operator, b) => {
   switch (operator) {
     case "+":
-      console.log(add(a, b));
+      return add(a, b);
       break;
     case "-":
-      console.log(subtract(a, b));
+      return subtract(a, b);
       break;
     case "*":
-      console.log(multiply(a, b));
+      return multiply(a, b);
       break;
     case "/":
-      console.log(divide(a, b));
+      return divide(a, b);
       break;
   }
 };
+
+const findValue = () => {
+  let arr = mainDisplay.textContent.split(" ");
+  let value;
+  if (arr.length < 3 || arr[arr.length - 1] === "") {
+    console.log(arr[0]);
+    value = arr[0];
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      opData = arr.splice(0, 3);
+      a = parseFloat(opData[0]);
+      operator = opData[1];
+      b = parseFloat(opData[2]);
+      value = operate(a, operator, b);
+      arr.unshift(value);
+    }
+    bgDisplay.textContent = mainDisplay.textContent;
+  }
+  mainDisplay.textContent = value;
+};
+
+//KeyBoard Logic
+const numKeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "."];
+const opKeys = ["+", "-", "/", "*"];
+
+whole.addEventListener("click", (e) => {
+  mainDisplay.focus();
+});
+
+mainDisplay.addEventListener("keydown", (e) => {
+  let x = e.code;
+  let arr = [
+    "Digit",
+    "Minus",
+    "Equal",
+    "Numpad",
+    "Backspace",
+    "Slash",
+    "Enter",
+  ];
+  if (arr.some((type) => x.includes(type))) {
+    if (numKeys.some((key) => e.key.includes(key))) {
+      mainDisplay.textContent = `${mainDisplay.textContent}${e.key}`;
+    } else if (opKeys.some((key) => e.key.includes(key))) {
+      const lastVal = mainDisplay.textContent.split(" ").at(-1);
+      if (lastVal == "") {
+        let freshArr = mainDisplay.textContent.split(" ");
+        freshArr.splice(-2, 1, e.target.textContent);
+        mainDisplay.textContent = freshArr.join(" ");
+      } else {
+        mainDisplay.textContent = `${mainDisplay.textContent} ${e.key} `;
+      }
+    } else if (e.key == "Backspace") {
+      mainDisplay.textContent = "";
+    } else {
+      findValue();
+    }
+  }
+});
 
 //Operator Events
 const numEventCreator = (btn) => {
@@ -86,4 +148,9 @@ clearBtn.addEventListener("click", () => {
 
 ceBtn.addEventListener("click", () => {
   mainDisplay.textContent = "";
+});
+
+// equals event and final logic
+equalsBtn.addEventListener("click", () => {
+  findValue();
 });
